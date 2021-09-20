eeecc19ee85f3d89ac6ef19024f7b28c6463c5fa
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2021-09-20 21:52:14 +0200

    set eventlistener for first window

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 document.getElementById("1").addEventListener("click", function() {
-    var leftPart =document.createElement("div");
-    leftPart.setAttribute("class","left-window");
-    var rightPart =document.createElement("div");
-    rightPart.setAttribute("class","right-window");
-    var window = document.getElementById("window-1");
-    var window = document.getElementById("window-1");
-    window.appendChild(rightPart)
-    leftPart.appendChild(window)
+  //zviditelnění "okenic"
+  document.getElementById("invisible-right-1").classList.add("visible-right")
+  document.getElementById("invisible-left-1").classList.add("visible-left")
+
+  //vyprázdnění obsahu buttonu
+  let element = document.getElementById("1");
+    while (element.firstChild) {
+  element.removeChild(element.firstChild);
+  }
+  document.getElementById("1").classList.add("empty")
+
+  var song = document.createElement("div");
+  songId="song-"+"1"
+  song.setAttribute("id",songId)
+  song.setAttribute("class","song-class")
+  song.innerHTML='<a href="https://www.youtube.com/watch?v=sE3uRRFVsmc">song</a>'
+
+  var photo =document.createElement("div");
+  photoId="photo"+"1"
+  photo.setAttribute("id",photoId)
+  photo.setAttribute("class","photo-class");
+  photo.innerHTML="<p>Day photo</p><br><p>Show</p>"
+
+  windowParent=document.getElementById("1")
+  windowParent.appendChild(song)
+  windowParent.appendChild(photo)
   }); 
